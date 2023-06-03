87fd91b0c2e4b2681d27d760e396c0d181a4242d
Carolina Correa <carolina.unila@gmail.com>
2023-06-02 22:25:57 -0300

    retirada de linhas comentadas desnecessarias

--- calculadora.js
+++ calculadora.js
@@ -1,49 +1,6 @@
 
 function criarCalculadora() {
-    //     var calculadoraDiv = document.getElementById("calculadora");
-
-    //     // Crie a estrutura HTML da calculadora
-    //     var calculadoraHTML = '<input type="text" id="numero" placeholder="Número">';
-    //     calculadoraHTML += '<select id="operacao">';
-    //     calculadoraHTML += '<option value="soma">Somar</option>';
-    //     calculadoraHTML += '<option value="subtracao">Subtrair</option>';
-    //     calculadoraHTML += '<option value="divisao">Dividir</option>';
-    //     calculadoraHTML += '<option value="multiplicacao">Multiplicar</option>';
-    //     calculadoraHTML += '</select>';
-    //     calculadoraHTML += '<button onclick="calcular()">Calcular</button>';
-    //     calculadoraHTML += '<div id="resultado"></div>';
-
-    //     // Adicione a estrutura HTML da calculadora ao elemento <div>
-    //     calculadoraDiv.innerHTML = calculadoraHTML;
-    // }
-
-    // function calcular() {
-    //     var numeros = document.getElementById("numero").value.split(",").map(function(num) {
-    //         return parseFloat(num.trim());
-    //     });
-    //     var operacao = document.getElementById("operacao").value;
-
-    //     var resultado;
-    //     if (operacao === "soma") {
-    //         resultado = numeros.reduce(function(acc, curr) {
-    //             return acc + curr;
-    //         });
-    //     } else if (operacao === "subtracao") {
-    //         resultado = numeros.reduce(function(acc, curr) {
-    //             return acc - curr;
-    //         });
-    //     } else if (operacao === "divisao") {
-    //         resultado = numeros.reduce(function(acc, curr) {
-    //             return acc / curr;
-    //         });
-    //     } else if (operacao === "multiplicacao") {
-    //         resultado = numeros.reduce(function(acc, curr) {
-    //             return acc * curr;
-    //         });
-    //     }
-
-    //     document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
-
+    
     var resultadoInput = document.getElementById("resultado");
     var numeroAtual = "";
     var operadorAtual = "";
